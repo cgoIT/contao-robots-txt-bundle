cbe4efe7b90735cc8b140ae8d1837034df29c81a
cgoIT <info@cgo-it.de>
2026-03-27 18:20:44 +0100

    chore: update copyright header

--- src/DependencyInjection/CgoitRobotsTxtExtension.php
+++ src/DependencyInjection/CgoitRobotsTxtExtension.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of cgoit\contao-robots-txt-bundle for Contao Open Source CMS.
  *
- * @copyright  Copyright (c) 2025, cgoIT
+ * @copyright  Copyright (c) 2026, cgoIT
  * @author     cgoIT <https://cgo-it.de>
  * @license    LGPL-3.0-or-later
  */
@@ -22,6 +22,6 @@ class CgoitRobotsTxtExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-        $loader->load('di.yml');
+        $loader->load('services.yml');
     }
 }
